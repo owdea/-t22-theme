731c00a6e26b0615c25b69effbadadd5c48d8bf2
Ondřej Pelcl <ondrejpel@seznam.cz>
2024-10-06 06:55:55 +0200

    IP-567 - changed settings of the Primary menu in the ACF plugin, deleted useless CSS classes

--- header.php
+++ header.php
@@ -6,6 +6,12 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<?php wp_head(); ?>
+    <style>
+        .primary-navigator:hover {
+            background-color: <?php echo get_field('selected_navigator_bg', 'option') ? get_field('selected_navigator_bg', 'option') : get_field_object('selected_navigator_bg', 'option')['default_value']; ?>;
+            color: <?php echo get_field('selected_navigator_text_color', 'option') ? get_field('selected_navigator_text_color', 'option') : get_field_object('selected_navigator_text_color', 'option')['default_value']; ?>;
+        }
+    </style>
 </head>
 
 <body <?php body_class(); ?>>
