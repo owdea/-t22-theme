389580ecb7ac48b673d277f904b22ada07a72ca4
Ondřej Pelcl <ondrejpel@seznam.cz>
2024-10-15 18:21:00 +0200

    IP577 - Added condition to the Link block (Option to enter either link or post object)

--- utils/blocks/link/link.php
+++ utils/blocks/link/link.php
@@ -1,30 +1,28 @@
 <?php
 /**
- * Block - Arrange Meeting
+ * Block - Link block (to post/page or external)
  *
  * @package ct22
  */
 
 $page_link = get_field('block_link_page');
-if ($page_link || (get_field('block_link_title') && get_field('block_link_link') && get_field('block_link_image'))) {
+if ($page_link || (get_field('block_link_title') && get_field('block_link_link') && get_field('block_link_image'))):
+    $title = get_field('block_link_title') ? get_field('block_link_title') : get_the_title($page_link->ID);
+    $link = get_field('block_link_custom') ? get_field('block_link_link') : get_permalink($page_link->ID);
+    $img = get_field('block_link_image') ? get_field('block_link_image') : get_the_post_thumbnail_url($page_link->ID, 'full');
+    ?>
+    <div class="block-link-component">
+        <a href="<?php echo $link?>">
+            <div>
+                <span>ODKAZ</span>
+                <br>
+                <h3><?php echo $title ?></h3>
+            </div>
+            <div class="block-link-img-container">
+                <img src="<?php echo $img ?>">
+            </div>
+        </a>
+    </div>
 
-$title = get_field('block_link_title') ? get_field('block_link_title') : get_the_title($page_link->ID);
-$link = get_field('block_link_link') ? get_field('block_link_link') : get_permalink($page_link->ID);;
-$img = get_field('block_link_image') ? get_field('block_link_image') : get_the_post_thumbnail_url($page_link->ID, 'full');
-
-?>
-<div class="block-link-component">
-    <a href="<?php echo $link?>">
-        <div>
-            <span>ODKAZ</span>
-            <br>
-            <h3><?php echo $title ?></h3>
-        </div>
-        <div class="block-link-img-container">
-            <img src="<?php echo $img ?>">
-        </div>
-    </a>
-</div>
-
-<?php
-}
+    <?php
+endif;
