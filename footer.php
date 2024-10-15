5ae10627b07f025f7c3c1b5f3bdb993c6f403d9a
Ondřej Pelcl <ondrejpel@seznam.cz>
2024-10-15 15:53:17 +0200

    IP569 - added conditional rendering for footer.php

--- footer.php
+++ footer.php
@@ -3,27 +3,39 @@
     <div class="footer-container">
         <a class="footer-logo">
             <?php $main_logo = get_field('footer_main_logo', 'options');?>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/acf/<?php echo $main_logo; ?>.svg" alt="Logo České Televize">
+            <img src="<?= $main_logo ? get_template_directory_uri() . '/assets/img/acf/' . $main_logo : get_template_directory_uri() . '/assets/img/acf/ct'; ?>.svg" alt="Logo České Televize">
         </a>
         <div class="footer-contact">
             <div class="footer-contact-center">
+                <?php
+                    if (get_field('footer_contact', 'options')['footer_contact_title'] && get_field('footer_contact', 'options')['footer_contact_open_hours']) :
+                ?>
                 <span class="font-semibold text-black mb-2 tablet:mb-0"><?php echo get_field('footer_contact', 'options')['footer_contact_title']; ?></span>
                 <br class="tablet:hidden">
                 <span class="tablet:mt-2"><?php echo get_field('footer_contact', 'options')['footer_contact_open_hours']; ?></span>
+                <?php
+                endif;
+                ?>
             </div>
             <div class="footer-contact-contacts-list">
                 <?php
                     $phone_number = get_field('footer_contact', 'options')['footer_contact_phone_number'];
                     $mail_address = get_field('footer_contact', 'options')['footer_contact_email'];
+                    if ($phone_number):
                 ?>
                 <a href="tel:<?php echo $phone_number;?>">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/phone.svg">
                     <span><?php echo $phone_number; ?></span>
                 </a>
+                <?php
+                    endif;
+                    if ($mail_address):
+                ?>
                 <a href="mailto:<?php echo $mail_address;?>">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/mail.svg">
                     <span><?php echo $mail_address; ?></span>
                 </a>
+                <?php endif; ?>
             </div>
         </div>
         <hr class="tablet:hidden">
@@ -48,15 +60,19 @@
                     $icon_hover = get_sub_field('social_network_active_icon');
                     $name = get_sub_field('social_network_name');
                     $link = get_sub_field('social_network_link');
-                    ?>
-                    <li>
-                        <a href="<?php echo $link ?>">
-                            <img id="socials-icon" src="<?php echo get_template_directory_uri(); ?>/assets/img/acf/<?php echo $icon; ?>.svg" alt="<?php $name = get_sub_field('social_network_name'); ?> ikona">
-                            <img id="socials-icon-hover" class="hidden" src="<?php echo get_template_directory_uri(); ?>/assets/img/acf/<?php echo $icon_hover; ?>.svg" alt="<?php $name = get_sub_field('social_network_name'); ?> aktivní ikona">
-                            <span><?php echo $name ?></span>
-                        </a>
-                    </li>
-                <?php endwhile; ?>
+                    if ($icon && $icon_hover && $name && $link):
+                ?>
+                <li>
+                    <a href="<?php echo $link ?>">
+                        <img id="socials-icon" src="<?php echo get_template_directory_uri(); ?>/assets/img/acf/<?php echo $icon; ?>.svg" alt="<?php $name = get_sub_field('social_network_name'); ?> ikona">
+                        <img id="socials-icon-hover" class="hidden" src="<?php echo get_template_directory_uri(); ?>/assets/img/acf/<?php echo $icon_hover; ?>.svg" alt="<?php $name = get_sub_field('social_network_name'); ?> aktivní ikona">
+                        <span><?php echo $name ?></span>
+                    </a>
+                </li>
+                <?php
+                    endif;
+                    endwhile;
+                ?>
             </ul>
         </div>
         <?php endif; ?>
@@ -80,13 +96,17 @@
             <?php while( have_rows('footer_logos', 'option') ): the_row();
                 $icon = get_sub_field('footer_tv_logo');
                 $link = get_sub_field('footer_tv_link');
-                ?>
-                <li>
-                    <a href="<?php $link ?>">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/acf/<?php echo $icon; ?>.svg" alt="">
-                    </a>
-                </li>
-            <?php endwhile; ?>
+                if ($icon):
+            ?>
+            <li>
+                <a href="<?=  $link ?: '' ?>">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/acf/<?php echo $icon; ?>.svg" alt="">
+                </a>
+            </li>
+            <?php
+                endif;
+                endwhile;
+            ?>
         </ul>
         <?php endif; ?>
     </div>
