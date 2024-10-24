b1ddba64d50f75380223a92cbefa59c2c6ffd09d
Ondřej Pelcl <ondrejpel@seznam.cz>
2024-10-24 10:34:32 +0200

    Added fallback img to Link block

--- utils/blocks/link/link.php
+++ utils/blocks/link/link.php
@@ -6,10 +6,16 @@
  */
 
 $page_link = get_field('block_link_page');
-if ($page_link || (get_field('block_link_title') && get_field('block_link_link') && get_field('block_link_image'))):
+if ($page_link || (get_field('block_link_title') && get_field('block_link_link'))):
     $title = get_field('block_link_title') ? get_field('block_link_title') : get_the_title($page_link->ID);
     $link = get_field('block_link_custom') ? get_field('block_link_link') : get_permalink($page_link->ID);
-    $img = get_field('block_link_image') ? get_field('block_link_image') : get_the_post_thumbnail_url($page_link->ID, 'full');
+    if (get_field('block_link_image')) {
+        $img = get_field('block_link_image');
+    } else if ($page_link && get_the_post_thumbnail_url($page_link->ID, 'full')){
+        $img = get_the_post_thumbnail_url($page_link->ID, 'full');
+    } else {
+        $img = get_template_directory_uri() . '/assets/img/placeholder.webp';
+    }
     ?>
     <div class="block-link-component">
         <a href="<?php echo $link?>">
@@ -17,9 +23,11 @@ if ($page_link || (get_field('block_link_title') && get_field('block_link_link')
                 <span>ODKAZ</span>
                 <h3><?php echo $title ?></h3>
             </div>
+            <?php if($img): ?>
             <div class="block-link-img-container">
-                <img src="<?php echo $img ?>">
+                <img src="<?php echo $img ?>" alt="obrázek týkající se <?php echo $title ?>">
             </div>
+            <?php endif; ?>
         </a>
     </div>
 
