9ff6a9e02bda6c8a1b79089f61709a5bf02adc1e
Ondřej Pelcl <ondrejpel@seznam.cz>
2024-10-30 15:26:41 +0100

    IP-608 - Added html structure and css for pinned article on taxonomy page

--- archive.php
+++ archive.php
@@ -11,19 +11,29 @@
             $pinned_post_title = $pinned_post->post_title;
             $pinned_post_url = get_permalink($pinned_post->ID);
             $pinned_post_excerpt = $pinned_post->post_excerpt;
-            $pinned_post_img = get_the_post_thumbnail_url($pinned_post) ?: get_template_directory_uri() . '/assets/img/placeholder.webp';
+            $pinned_post_img = get_the_post_thumbnail_url($pinned_post, 'full') ?: get_template_directory_uri() . '/assets/img/placeholder.webp';
             $pinned_post_sources = get_field('article_sources', $pinned_post->ID);
-            echo $pinned_post_title;
-            echo $pinned_post_url;
-            echo $pinned_post_excerpt;
-            echo $pinned_post_img;
-            echo $pinned_post_sources;
         } else {
             the_post();
+            $pinned_post_title = get_the_title();
+            $pinned_post_url = get_permalink();
+            $pinned_post_excerpt = get_the_excerpt();
+            $pinned_post_img = get_the_post_thumbnail_url(null, 'full') ?: get_template_directory_uri() . '/assets/img/placeholder.webp';
+            $pinned_post_sources = get_field('article_sources');
         }
-
-
         ?>
+        <div class="pinned-post">
+            <a href="<?php echo $pinned_post_url ?>">
+                <div class="pinned-post-img">
+                    <img src="<?php echo $pinned_post_img?>" alt="<?php echo $pinned_post_title ?>">
+                </div>
+                <div class="pinned-post-info">
+                    <h2><?php echo $pinned_post_title ?></h2>
+                    <span><?php echo $pinned_post_excerpt ?></span>
+                    <span><?php echo $pinned_post_sources ?></span>
+                </div>
+            </a>
+        </div>
 
     </div>
 <?php endif; ?>
