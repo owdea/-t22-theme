0d0794e81f0db31daf6f799a483932a09c34a6f1
Ondřej Pelcl <ondrejpel@seznam.cz>
2024-11-12 19:18:21 +0100

    IP-601 - fixed if statement in pinned block post

--- template-parts/page-content/pinned-post.php
+++ template-parts/page-content/pinned-post.php
@@ -12,27 +12,28 @@
  */
 
 if (!empty($args)):
-$pinned_post_url = $args['post_url'] ?? null;
-$pinned_post_img = $args['post_img'] ?? null;
-$pinned_post_title = $args['post_title'] ?? null;
-$pinned_post_excerpt = $args['post_excerpt'] ?? null;
-$pinned_post_sources = $args['post_sources'] ?? null;
-if ($pinned_post_url && $pinned_post_img) {}
-?>
+    $pinned_post_url = $args['post_url'] ?? null;
+    $pinned_post_img = $args['post_img'] ?? null;
+    $pinned_post_title = $args['post_title'] ?? null;
+    $pinned_post_excerpt = $args['post_excerpt'] ?? null;
+    $pinned_post_sources = $args['post_sources'] ?? null;
+    if ($pinned_post_url && $pinned_post_img && $pinned_post_title):
+        ?>
 
-<article class="template-part-pinned-post">
+        <article class="template-part-pinned-post">
 
-    <a href="<?php echo esc_url($pinned_post_url); ?>">
-        <div class="pinned-post-img">
-            <img src="<?php echo esc_url($pinned_post_img); ?>" alt="<?php echo esc_attr($pinned_post_title); ?>">
-        </div>
-        <div class="pinned-post-info">
-            <h2><?php echo esc_html($pinned_post_title); ?></h2>
-            <span><?php echo esc_html($pinned_post_excerpt); ?></span>
-            <span><?php echo esc_html($pinned_post_sources); ?></span>
-        </div>
-    </a>
-</article>
-<?php
+            <a href="<?php echo esc_url($pinned_post_url); ?>">
+                <div class="pinned-post-img">
+                    <img src="<?php echo esc_url($pinned_post_img); ?>" alt="<?php echo esc_attr($pinned_post_title); ?>">
+                </div>
+                <div class="pinned-post-info">
+                    <h2><?php echo esc_html($pinned_post_title); ?></h2>
+                    <span><?php echo esc_html($pinned_post_excerpt); ?></span>
+                    <span><?php echo esc_html($pinned_post_sources); ?></span>
+                </div>
+            </a>
+        </article>
+        <?php
+    endif;
 endif;
 ?>
